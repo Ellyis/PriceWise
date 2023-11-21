2822cf3095ff0a1da71b413393c944363b765135
Ellyis <kaibinyong2002@gmail.com>
2023-11-21 11:07:49 +0800

    modified maxDuration

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { EmailProductInfo } from "@/types";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300; // 5 minutes
+export const maxDuration = 10; // 10 seconds
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
